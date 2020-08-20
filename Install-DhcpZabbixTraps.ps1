311060dac39c3adb9805d0965c9b3db1398be1e5
Rory Fewell <roryfewell@hotmail.com>
2020-08-20 10:11:48 +0100

    Bugfix: Correct task action in installer

--- Install-DhcpZabbixTraps.ps1
+++ Install-DhcpZabbixTraps.ps1
@@ -64,7 +64,7 @@ $dhcpLeaseActionArgs =
         "-NoProfile",
         "-NoLogo",
         "-File",
-        "`"$ZabbixRoot`"",
+        "`"$dhcpLeaseFilePath`"",
         "-ZabbixIP",
         $ZabbixIP,
         "-ComputerName",
